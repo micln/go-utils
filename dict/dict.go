cb0b39be2e625fe75167fa8320bbfa515d126e66
zhanghuiren <zhrlnt@gmail.com>
2017-02-28 00:56:15 +0800

    dict

---
+++ dict/dict.go
@@ -0,0 +1,108 @@
+package dict
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/micln/go-utils"
+)
+
+type dict struct {
+	provider map[string]interface{}
+}
+
+func NewDict() *dict {
+	return &dict{
+		provider: newMap(),
+	}
+}
+
+func (d *dict) Get(k interface{}) interface{} {
+	paths := strings.Split(toString(k), `.`)
+
+	var current interface{}
+	current = toMap(d.provider)
+
+	size := len(paths)
+	for i := 0; i < size-1; i++ {
+		m := toMap(current)
+		current = m[paths[i]]
+	}
+
+	return toMap(current)[paths[size-1]]
+}
+
+func (d *dict) Set(k interface{}, v interface{}) {
+	paths := strings.Split(toString(k), `.`)
+
+	parent := d.provider
+
+	size := len(paths)
+	for idx := 0; idx < size-1; idx++ {
+		//fmt.Println(idx, d.provider, parent, &d.provider, &parent)
+		seq := paths[idx]
+
+		i := parent[seq]
+		if _, ok := i.(map[string]interface{}); !ok {
+			parent[seq] = newMap()
+			parent = parent[seq].(map[string]interface{})
+		} else {
+			parent = i.(map[string]interface{})
+		}
+	}
+
+	parent[paths[size-1]] = v
+}
+
+func (d *dict) Forget(k interface{}) {
+	d.Set(k, nil)
+}
+
+func (d *dict) parseJson(json string) {
+
+}
+
+func (d *dict) Keys() (keys []string) {
+	for k := range d.provider {
+		keys = append(keys, k)
+	}
+	return
+}
+
+func (d *dict) Values() (values []interface{}) {
+	for _, v := range d.provider {
+		values = append(values, v)
+	}
+	return
+}
+
+func (d *dict) Json() string {
+	return go_utils.JsonEncode(d.provider)
+}
+
+func toString(k interface{}) string {
+	switch k.(type) {
+	case string:
+		return k.(string)
+	case int:
+		return strconv.FormatInt(int64(k.(int)), 10)
+	default:
+		return fmt.Sprint(k)
+	}
+}
+
+func newMap() map[string]interface{} {
+	return make(map[string]interface{})
+}
+
+func toMap(i interface{}) map[string]interface{} {
+	m, ok := i.(map[string]interface{})
+	if !ok {
+		//fmt.Printf("%v,%v\n", i, m)
+		m = make(map[string]interface{})
+		//fmt.Printf("%v,%v\n", i, m)
+	}
+
+	return m
+}
---
+++ dict/dict_test.go
@@ -0,0 +1,105 @@
+package dict
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
+	"testing"
+)
+
+func TestNewDict(t *testing.T) {
+	d := NewDict()
+
+	assert(t, d.Get(`a.b.c.d.e`), nil)
+
+	d.Set(`a.b.c.d.e`, `ooo`)
+	assert(t, d.Get(`a.b.c.d.e`), `ooo`)
+
+	d.Set(`a.b.c.d.e`, 4324)
+	assert(t, d.Get(`a.b.c.d.e`), 4324)
+
+	d.Set(78, 88)
+	assert(t, d.Get(78), 88)
+
+	d.Forget(78)
+	assert(t, d.Get(78), nil)
+
+}
+
+func TestDict_Json(t *testing.T) {
+	d := NewDict()
+
+	d.Set(1, 3)
+	d.Set(`a`, 'b')
+	d.Set(`a.b.c.d`, 'e')
+
+	assert(t, d.Json(), `{"1":3,"a":{"b":{"c":{"d":101}}}}`)
+	assert(t, d.Keys(), []string{`1`, `a`})
+	assert(t, d.Values(), []interface{}{
+		3,
+		map[string]interface{}{
+			`b`: map[string]interface{}{
+				`c`: map[string]interface{}{
+					`d`: 'e',
+				},
+			},
+		},
+	})
+}
+
+func assert(t *testing.T, a interface{}, b interface{}) {
+	if fmt.Sprint(a) != fmt.Sprint(b) {
+		file, line := calledBy()
+		t.Errorf(
+			"Failure in %s:%d\nexpect:\t%v\n   but:\t%v\n----\n%s\n",
+			file, line,
+			a, b,
+			showFile(file, line),
+		)
+	}
+}
+
+func calledBy() (string, int) {
+	_, file, line, _ := runtime.Caller(2)
+	return file, line
+	file = strings.TrimPrefix(file, os.Getenv(`GOPATH`))
+	return `$GOPATH` + file, line
+}
+
+func showFile(filename string, line int) string {
+	file, _ := ioutil.ReadFile(filename)
+	buf := bytes.NewBufferString(``)
+
+	size := len(file)
+	begin := line - 2
+	end := line + 3
+
+	//	@todo line超过文件时
+	found := 1
+	for idx := 0; idx < size; idx++ {
+		if found >= begin && found < end {
+			buf.WriteByte(file[idx])
+		}
+
+		if file[idx] == '\n' {
+			found++
+
+			if found >= begin && found < end {
+				tag := ' '
+				if found == line {
+					tag = '#'
+				}
+				buf.WriteString(fmt.Sprintf("%c%4d|\t", tag, found))
+			}
+
+			if found >= end {
+				break
+			}
+		}
+	}
+
+	return buf.String()
+}
